44555fb07acfcbb5bef36e1351ae4144c056fa1f
kaushikkts <105159350+kaushikkts@users.noreply.github.com>
2025-02-15 16:27:03 +0530

    Testing multi delimiter change

--- index.test.js
+++ index.test.js
@@ -2,7 +2,11 @@ const addStringNumbers = require('./index');
 
 describe('String calculator', () => {
 
-    test('should handle delimiter change', () => {
+    test('should handle single delimiter change', () => {
         expect(addStringNumbers('//;\n1;2')).toStrictEqual([1,2]);
+    });
+
+    test('should handle multi delimiter change', () => {
+        expect(addStringNumbers('//;;;;\n1;;;;2')).toStrictEqual([1,2]);
     });
 });
